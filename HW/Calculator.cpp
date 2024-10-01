ce1ede7e78a13c396e8be2b93739e80ef0248210
JHubO420 <ijkl64818@gmail.com>
2024-10-01 11:27:52 +0900

    과제#1

--- HW/Calculator.cpp
+++ HW/Calculator.cpp
@@ -27,36 +27,3 @@ void Div::setValue(int x, int y) {
 int Div::calculate() {
 	return a / b;
 }
-//int main() {
-//	Add a;
-//	Sub s;
-//	Mul m;
-//	Div d;
-//
-//	while (true) {
-//		cout << "두 정수와 연산자를 입력하세요>>";
-//		int x, y;
-//		char op;
-//		cin >> x >> y >> op;
-//		if (op == '+') {
-//			a.setValue(x, y);
-//			a.calculate();
-//			cout << a.calculate() << endl;
-//		}
-//		else if (op == '-') {
-//			s.setValue(x, y);
-//			s.calculate();
-//			cout << s.calculate() << endl;
-//		}
-//		else if (op == '*') {
-//			m.setValue(x, y);
-//			m.calculate();
-//			cout << m.calculate() << endl;
-//		}
-//		else if (op == '/') {
-//			d.setValue(x, y);
-//			d.calculate();
-//			cout << d.calculate() << endl;
-//		}
-//	}
-//}
